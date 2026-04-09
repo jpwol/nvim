1b25d078db1a855b494f87f2aded39c9b07dbc80
jpwol <jpwolfe1981@gmail.com>
2026-04-09 04:19:48 -0400

    style: added borders :)

--- plugin/oil.lua
+++ plugin/oil.lua
@@ -9,10 +9,22 @@ require("oil").setup({
     buflisted = false,
     bufhidden = 'hide',
   },
-
   lsp_file_methods = {
     enabled = true,
   },
+  float = {
+    max_width = 0.7,
+    max_height = 0.7,
+    border = 'rounded',
+  },
+  confirmation = {
+    max_width = 0.5,
+    max_height = 0.5,
+    border = 'rounded',
+  },
+  keymaps_help = {
+    border = 'rounded'
+  }
 })
 
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })

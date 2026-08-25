a9721b2df75f67b187a44162d9c22ee10b780ad0
jpwol <jpwolfe1981@gmail.com>
2026-08-25 04:01:12 -0400

    chore(treesitter): added treesitter-context for block view

--- plugin/treesitter.lua
+++ plugin/treesitter.lua
@@ -3,3 +3,11 @@ vim.api.nvim_create_autocmd('FileType', {
     pcall(vim.treesitter.start)
   end
 })
+
+require('treesitter-context').setup({
+  enable = true,
+  multiwindow = true,
+  max_lines = 2,
+  min_window_height = 0,
+  line_numbers = true,
+})

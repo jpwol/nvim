df0f34c5112c07a416e45ac3a2e3a1995db5761c
jpwol <jpwolfe1981@gmail.com>
2026-08-25 04:00:24 -0400

    chore(treesitter): added treesitter-context for block view

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -11,6 +11,7 @@ vim.pack.add({
   { src = "https://codeberg.org/evergarden/nvim.git", name = 'evergarden' },
   -- TREESITTER
   { src = "https://github.com/nvim-treesitter/nvim-treesitter" },
+  { src = "https://github.com/nvim-treesitter/nvim-treesitter-context" },
   -- LSP
   { src = "https://github.com/neovim/nvim-lspconfig" },
   { src = "https://github.com/mason-org/mason.nvim" },
